9d5143dfe4a51e97719f772574410b89f929c556
Eric <ecshih@users.noreply.github.com>
2015-09-16 18:20:05 -0400

    moved new task entry to directive, added directive to timer page

---
+++ js/directives/entryForm.js
@@ -0,0 +1,11 @@
+app.directive('entryForm', function(){
+	return{
+		restrict: 'E',
+
+		/* function($scope, schedule){
+						$scope.newBlock = {};
+						$scope.schedule = schedule;
+					},*/
+		templateUrl: 'js/directives/entryForm.html'
+	};
+});
